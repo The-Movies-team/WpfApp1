e604ffcb98d687d9c0f3b0ea1eba9faf270df195
nicolai jensen <nbje63269@edu.ucl.dk>
2024-08-21 14:15:01 +0200

    Scenarie 2 GUI cirka lavet, mangler kode

---
+++ WpfApp1/Views/CinemaWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Views
+{
+    /// <summary>
+    /// Interaction logic for CinemaWindow.xaml
+    /// </summary>
+    public partial class CinemaWindow : Window
+    {
+        public CinemaWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddToCinemaList add = new AddToCinemaList();
+            add.Show();
+            
+        }
+    }
+}
---
+++ WpfApp1/Views/Startscreen.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using WpfApp1.Views;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Startscreen.xaml
+    /// </summary>
+    public partial class Startscreen : Window
+    {
+        public Startscreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddMovieWindow addMovieWindow = new AddMovieWindow();
+            addMovieWindow.Show();
+            this.Close();
+        }
+
+        private void btncinemaprogram_Click(object sender, RoutedEventArgs e)
+        {
+            CinemaWindow cinemaWindow = new CinemaWindow();
+            cinemaWindow.Show();
+            this.Close();
+        }
+    }
+}
